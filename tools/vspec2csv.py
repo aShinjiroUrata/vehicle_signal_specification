cd69a2460ec1d2fc07c5340667180630f0aae26e
shinjiro.urata <shinjiro.urata@access-company.com>
2016-12-20 09:21:54 +0900

    Modified csv conversion tool - added '--nobranch' option to remove branches from output csv - added '--row1pos1only' option to remove duplicative lines from output csv

--- tools/vspec2csv.py
+++ tools/vspec2csv.py
@@ -9,6 +9,7 @@ import vspec
 import json
 import getopt
 import csv
+import re
 
 def usage():
     print "Usage:", sys.argv[0], "[-I include_dir] ... [-i prefix:id_file:start_id] vspec_file csv_file"
@@ -17,6 +18,11 @@ def usage():
     print
     print "  -i prefix:id_file:start_id  Add include directory to search for included vspec"
     print "                              files. Can be used multiple timees."
+    print
+    print "  --nobranch                  Omit 'branches' and output 'leaves' only."
+    print
+    print "  --row1pos1only              Omit RowX, PosX items except for Row1 and Pos1"
+    print "                              to reduce duplicative lines."
     print
     print " vspec_file                   The vehicle specification file to parse."
     print " csv_file                    The file to output the CSV data to."
@@ -46,16 +52,22 @@ def format_data(json_data):
         Enum = '"' + ' / '.join(json_data['enum']) + '"'
     return Id + ","+ Type + ","+ Unit + ","+ Min + ","+ Max + ","+ Desc + ","+ Enum
 
-def json2csv(json_data, file_out, parent_signal):
+def json2csv(json_data, file_out, parent_signal, _nobranch, _row1pos1only):
     for k in json_data.keys():
+        if (_row1pos1only == True):
+            pattern = r"Row[2-9]|Pos[2-9]"
+            if (re.match(pattern, k)):
+                continue
+
         if (len(parent_signal) > 0):
             signal = parent_signal + "." + k
         else:
             signal = k
 
         if (json_data[k]['type'] == 'branch'):
-            file_out.write(signal + ',' + format_data(json_data[k]) + '\n')
-            json2csv(json_data[k]['children'], file_out, signal)
+            if (_nobranch == False):
+                file_out.write(signal + ',' + format_data(json_data[k]) + '\n')
+            json2csv(json_data[k]['children'], file_out, signal, _nobranch, _row1pos1only)
         else:
             file_out.write(signal + ',' + format_data(json_data[k]) + '\n')
 
@@ -63,7 +75,9 @@ if __name__ == "__main__":
     # 
     # Check that we have the correct arguments
     #
-    opts, args= getopt.getopt(sys.argv[1:], "I:i:")
+    opts, args= getopt.getopt(sys.argv[1:], "I:i:", ['nobranch', 'row1pos1only'])
+    nobranch = False
+    row1pos1only = False
 
     # Always search current directory for include_file
     include_dirs = ["."]
@@ -78,10 +92,14 @@ if __name__ == "__main__":
 
             [prefix, file_name, start_id] = id_spec
             vspec.db_mgr.create_signal_db(prefix, file_name, int(start_id))
+        elif o == "--nobranch":
+            nobranch = True
+        elif o == "--row1pos1only":
+            row1pos1only = True
         else:
             usage()
 
-    if len(args) != 2:
+    if len(args) < 2:
         usage()
 
     csv_out = open (args[1], "w")
@@ -92,6 +110,6 @@ if __name__ == "__main__":
         print "Error: {}".format(e)
         exit(255)
 
-    json2csv(tree, csv_out, "")
+    json2csv(tree, csv_out, "", nobranch, row1pos1only)
     csv_out.write("\n")
     csv_out.close()
